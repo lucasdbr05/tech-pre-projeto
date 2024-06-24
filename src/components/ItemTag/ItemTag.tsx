4d19ca3b93be266cd311d83c85da474927ce915a
AndreRimes <andre.2004.rimes@gmail.com>
2024-06-24 11:20:19 -0300

    uso do truncate

--- src/components/ItemTag/ItemTag.tsx
+++ src/components/ItemTag/ItemTag.tsx
@@ -1,39 +1,22 @@
-import React, { useState, useEffect } from "react";
 import cartIcon from "../../../public/shopping-cart (1).png";
 import Image from "next/image";
 
 export default function ItemTag({ isBig, hovered, title, price }: { isBig: boolean, hovered: boolean, title: String, price: number }) {
-    const [sliceSize, setSliceSize] = useState(13);
-
-    useEffect(() => {
-        const handleResize = () => {
-            const screenWidth = window.innerWidth;
-            if (screenWidth < 640) {
-                setSliceSize(isBig ? 10 : 5);
-            } else if (screenWidth >= 640 && screenWidth < 768) {
-                setSliceSize(isBig ? 13 : 3);
-            } else {
-                setSliceSize(isBig ? 20 : 14);
-            }
-        };
-
-        handleResize(); 
-    }, []);
 
     return (
         <div className={`absolute bg-[#0a0a0b] border-[#71717A] 
-            border-2 p-1 ${!isBig ? "w-[90%] xl:w-[55%] h-1/6  top-[75%] left-[5%] xl:left-[40%]" : "w-1/3 h-[10%] top-[80%] left-[60%]"} 
+            border-2 p-1 ${!isBig ? "max-w-[90%] xl:max-w-[55%] h-1/6 top-[75%] left-[5%] xl:left-[40%]" : "max-w-[33%] h-[10%] top-[80%] left-[60%]"} 
             rounded-3xl flex items-center justify-between flex-row`}>
-            <h1 className="px-3 whitespace-nowrap">
-                {title.length > sliceSize ? title.slice(0, sliceSize) + "..." : title}
+            <h1 className="px-3 truncate">
+                {title}
             </h1>
-            <div className={`rounded-2xl w-1/3 h-full bg-primary text-lg font-semibold 
+            <div className={`rounded-2xl w-1/3 min-w-[50px] h-full bg-primary text-lg font-semibold 
                 flex items-center justify-center ${hovered ? "animate-flip" : ""}`}>
                 {hovered ? (
                     <Image
                         src={cartIcon}
                         alt="cart icon"
-                        className="w-7 h-7 animate-flip cursor-pointer"
+                        className="w-6 h-6 lg:w-7 lg:h-7 animate-flip cursor-pointer"
                         width={100}
                         height={100}
                     />
